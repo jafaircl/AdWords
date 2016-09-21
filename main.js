19bfcefa681b5a7852d082bd338793f959291e5a
jafaircl <jafaircl@me.com>
2016-09-21 13:45:02 -0400

    Update main.js

--- main.js
+++ main.js
@@ -5,10 +5,10 @@ var JAMs = JAMs || {
     jStat: 'https://cdnjs.cloudflare.com/ajax/libs/jstat/1.5.3/jstat.min.js'
   },
   use: function ( url ) {
-    return eval( UrlFetchApp.fetch( url ).getContentText() );
+    eval( UrlFetchApp.fetch( url ).getContentText() );
   },
   test: function () {
-    eJAMs.use( JAMs.dependencies.jStat );
+    JAMs.use( JAMs.dependencies.jStat );
     var test = jStat.betaln(10, 155);
     Logger.log( test );
   }
